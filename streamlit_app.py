a85d02a8f30eaa67f7c65532cebb47f427f66949
Vaishprog <Vaishp242@gmail.com>
2024-11-07 01:28:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 # Import python packages
 import streamlit as st
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 # Write directly to the app
@@ -16,9 +18,10 @@ st.write('the current movie title is',title)
 #session = get_active_session()
 cnx=st.connection("snowflake")
 session=cnx.session()
+st.text(fruityvice_response.json)
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
-
+fv_df=st.dataframe(data=fruityvice_response.json(),use_container_width=True)
 ingredients_list=st.multiselect('Choose up to 5 ingredients:',my_dataframe, max_selections=5 )
 
 if ingredients_list:
