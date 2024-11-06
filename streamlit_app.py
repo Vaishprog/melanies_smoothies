4ce940e5012c2b72b762692cf3868b8c575ac039
Vaishprog <Vaishp242@gmail.com>
2024-11-07 02:18:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -74,3 +74,10 @@ if ingredients_list:
         st.subheader(fruit_chosen + 'Nutrition Information')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
         fv_df=st.dataframe(data=fruityvice_response.json(),use_container_width=True)
+         my_insert_stmt = """ insert into smoothies.public.orders(name_on_order,ingredients)
+            values ('"""+title+"""','"""+ingredients_string+"""')"""
+        st.write(my_insert_stmt)
+        time_to_insert=st.button('Submit Order')
+        if time_to_insert:
+            session.sql(my_insert_stmt).collect()
+            st.success('Your Smoothie is ordered!', icon="✅")
