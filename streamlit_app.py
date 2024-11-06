655f0485610c8f8df38d5c52e331a45842dc9e6a
Vaishprog <Vaishp242@gmail.com>
2024-11-07 01:44:08 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
-# Import python packages
+'''# Import python packages
 import streamlit as st
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 # Write directly to the app
@@ -18,10 +18,10 @@ st.write('the current movie title is',title)
 #session = get_active_session()
 cnx=st.connection("snowflake")
 session=cnx.session()
-st.text(fruityvice_response.json)
+
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
-fv_df=st.dataframe(data=fruityvice_response.json(),use_container_width=True)
+st.dataframe(data=my_dataframe, use_container_width=True)
+
 ingredients_list=st.multiselect('Choose up to 5 ingredients:',my_dataframe, max_selections=5 )
 
 if ingredients_list:
@@ -38,3 +38,28 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
+
+        '''
+
+import streamlit as st
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+
+fv_df=st.dataframe(data=fruityvice_response.json(),use_container_width=True)
+# Write directly to the app
+st.title("Customize Your Smoothie!:cup_with_straw:")
+st.write(
+    """
+    Choose the fruits you want in your custom smoothie.
+    """
+)
+
+title=st.text_input('Name on Smothie')
+st.write('the current movie title is',title)
+#st.text(fruityvice_response.json())
+if ingredients_list:
+    ingredients_string=''
+     for  x in ingredients_list:
+        ingredients_string += x
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fv_df=st.dataframe(data=fruityvice_response.json(),use_container_width=True)
