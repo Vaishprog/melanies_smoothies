e2b3522b8ca649e2669960524a6a6d2ce3d9effc
Vaishprog <Vaishp242@gmail.com>
2024-11-07 19:28:16 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -81,3 +81,36 @@ if ingredients_list:
         if time_to_insert:
             session.sql(my_insert_stmt).collect()
             st.success('Your Smoothie is ordered!', icon="✅")
+
+title=st.text_input('Name on Smothie')
+st.write('the current movie title is',title)
+
+#session = get_active_session()
+cnx=st.connection("snowflake")
+session=cnx.session()
+
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'))
+st.dataframe(data=my_dataframe, use_container_width=True)
+
+pd_df=my_dataframe.to_pandas()
+#st.dataframe(pd_df)
+#st.stop()
+ingredients_list=st.multiselect('Choose up to 5 ingredients:',my_dataframe, max_selections=5 )
+
+if ingredients_list:
+    #st.write(ingredients_list)
+    #st.text(ingredients_list)
+    ingredients_string=''
+    for  x in ingredients_list:
+        ingredients_string += x + ' '
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == x, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', x,' is ', search_on, '.')
+        st.subheader(x+ 'Nutrition information')
+        fruityvice_response=request.get("https://fruityvice.com/api/fruit/"+ x)
+        my_insert_stmt = """ insert into smoothies.public.orders(name_on_order,ingredients) values ('"""+title+"""','"""+ingredients_string+"""')"""
+        st.write(my_insert_stmt)
+        time_to_insert=st.button('Submit Order')
+        if time_to_insert:
+            session.sql(my_insert_stmt).collect()
+            st.success('Your Smoothie is ordered!', icon="✅")
+
